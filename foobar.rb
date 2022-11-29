06fb5eded9cd147f27f5a900bb0a4af65c5694b3
Venjaminnn <victoriagalchenko@gmail.com>
2022-11-29 17:21:20 +0300

    Foobar method

---
+++ foobar.rb
@@ -0,0 +1,12 @@
+puts "Укажите первое число:"
+number1 = gets.to_i
+puts "Укажите второе число:"
+number2 = gets.to_i
+
+if number1 == 20 || number2 == 20
+  puts number2
+else
+  puts number1 + number2
+end
+
+
