03723aeaa1f746af9743e76d81a7a860e9e2ac47
Venjaminnn <victoriagalchenko@gmail.com>
2022-11-29 17:59:52 +0300

    Change code to methods

--- foobar.rb
+++ foobar.rb
@@ -1,12 +1,13 @@
-puts "Укажите первое число:"
-number1 = gets.to_i
-puts "Укажите второе число:"
-number2 = gets.to_i
+def foobar
+  puts "Укажите первое число:"
+  number1 = gets.to_i
+  puts "Укажите второе число:"
+  number2 = gets.to_i
 
-if number1 == 20 || number2 == 20
-  puts number2
-else
-  puts number1 + number2
+  if number1 == 20 || number2 == 20
+    puts number2
+  else
+    puts number1 + number2
+  end
 end
-
 
--- greeting.rb
+++ greeting.rb
@@ -1,13 +1,14 @@
-puts "Укажите Ваше имя:"
-name = gets.chomp
-puts "Укажите Вашу фамилию:"
-surname = gets.chomp
-puts "Укажите Ваш возраст:"
-age = gets.to_i
+def greeting
+  puts "Укажите Ваше имя:"
+  name = gets.chomp
+  puts "Укажите Вашу фамилию:"
+  surname = gets.chomp
+  puts "Укажите Ваш возраст:"
+  age = gets.to_i
 
-if age < 18
-  puts "Привет, #{name} #{surname}. Тебе меньше 18 лет, но начать учиться программировать никогда не рано."
-else
-  puts "Привет, #{name} #{surname}. Самое время заняться делом!"
+  if age < 18
+    puts "Привет, #{name} #{surname}. Тебе меньше 18 лет, но начать учиться программировать никогда не рано."
+  else
+    puts "Привет, #{name} #{surname}. Самое время заняться делом!"
+  end
 end
-
