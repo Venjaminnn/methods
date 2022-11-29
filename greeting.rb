337fce4a5b9165a16afe76945e0d1242aa94eaf7
Venjaminnn <victoriagalchenko@gmail.com>
2022-11-29 17:19:12 +0300

    Greeting method

---
+++ greeting.rb
@@ -0,0 +1,13 @@
+puts "Укажите Ваше имя:"
+name = gets.chomp
+puts "Укажите Вашу фамилию:"
+surname = gets.chomp
+puts "Укажите Ваш возраст:"
+age = gets.to_i
+
+if age < 18
+  puts "Привет, #{name} #{surname}. Тебе меньше 18 лет, но начать учиться программировать никогда не рано."
+else
+  puts "Привет, #{name} #{surname}. Самое время заняться делом!"
+end
+
